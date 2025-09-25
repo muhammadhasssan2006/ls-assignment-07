42aadfcc2c5f69dff3d32f637f9956ae84549629
muhammadhasssan2006 <hassanweb365@gmail.com>
2025-09-25 16:38:46 +0500

    more done

--- app.js
+++ app.js
@@ -86,15 +86,120 @@
 
 // Question:06:-
 
-function friction(num){
-    var result = Math.round(num);
-    return result;
+// function friction(num){
+//     var result = Math.round(num);
+//     return result;
 
-}
+// }
 
-var userINput = +prompt("Enter friction value");
+// var userINput = +prompt("Enter friction value");
 
-var getValue = friction(userINput);
-console.log(,getValue);
+// var getValue = friction(userINput);
+// console.log(getValue);
+
+
+// Question:07:-
+
+// function counting(num1,num2){
+//     for(var i = num1;i <= num2;i++){
+
+//         console.log(i);
+//     }
+
+// }
+
+// counting(+prompt("Enter starting number"),+prompt("Enter ending number"));
+
+
+// Question:08:-
+
+
+// function hyp(num1,num2){
+//     var hyp = num1*num1 + num2*num2;
+//     hyp = Math.sqrt(hyp);
+//     return hyp;
+
+// }
+
+// var base = +prompt("Enter value of base")
+// var perpendicular = +prompt("Enter value of perpendicular")
+
+// var getValue = hyp(base,perpendicular);
+
+// console.log("Hypotenuse is: ",getValue);
+
+
+
+
+// Question:09:-
+
+// function rectangle(num1,num2){
+//     var rangle = num1*num2;
+//     return rangle;
+
+// }
+
+
+// var value = +prompt("Enter value of width");
+// var variable = +prompt("Enter value of height");
+
+// var getValue = rectangle(value,variable);
+
+// console.log("Area of rectangle is: ",getValue);
+
+
+
+
+// Question:10:-
+
+// function palindrome(userINput){
+//     var len = userINput.length;
+//     var reverse = "";
+//     for(var i = len -1; i >=0;i-- ){
+//         reverse = reverse + userINput[i]
+//     }
+//     if(reverse === userINput){
+//         console.log("Given word is palindrome ");
+
+//     }
+//     else{
+//         console.log("Given word is not palindrome ");
+
+//     }
+
+// }
+
+// palindrome(prompt("Enter word to check palindrome "));
+
+
+
+
+// Question:11:-
+
+// function upperCase(str){
+//     var upertext = str.slice(0,1).toUpperCase();
+//     var finalText = str.slice(1).toLowerCase();
+//     var finalText = upertext+finalText;
+
+//     return finalText;
+
+// }
+
+
+// var userINput = prompt("Enter a word or sentance");
+// var getValue = upperCase(userINput);
+// console.log(getValue);
+
+
+
+
+
+
+
+
+
+
+
+
 
 
