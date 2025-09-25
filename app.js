7c3bf5ef86b9688009e78939ba745d9fc61972a3
muhammadhasssan2006 <hassanweb365@gmail.com>
2025-09-25 13:19:38 +0500

    more added

--- app.js
+++ app.js
@@ -1,1 +1,75 @@
-// -------------------------------------chapter 35-
+// -------------------------------------chapter 35-38 ---------------------------------
+
+// Question:01:-
+
+// function date(){
+//  var date = new Date();
+//  console.log(date);
+ 
+// }
+
+// date();
+
+
+// Question:02:-
+
+
+// function Name(firstName,lastName){
+//     var fullName = firstName+" "+lastName;
+//     console.log(fullName);
+    
+
+// }
+
+// Name("Hassan","Khan")
+
+
+// Question:03:-
+
+// function sum(a,b,c){
+//     var add = a+b+c;
+//     return add;
+// }
+
+// var getValue = sum(5,10,15);
+// console.log(getValue);
+
+// Question:04:-
+
+function calculator(num1,operator,num2){
+  var result;
+  if(operator == "+"){
+    result = num1+num2
+  }  
+  else if(operator == "-"){
+    result = num1-num2
+
+  } 
+   else if(operator == "*"){
+    result = num1*num2
+
+
+  } 
+   else if(operator == "/"){
+    if(num2 !== 0){
+        result = num1/num2
+    }
+    else{
+    result = "division is 0"
+
+    }
+
+  } 
+  else{
+    console.log("operator is invalid");
+  }
+  return result;
+}
+
+var getValue = calculator(+prompt("Enter number 01"),prompt("Enter operator (+,-,*/)"),+prompt("Enter number 02"));
+
+console.log(getValue);
+
+
+
+
